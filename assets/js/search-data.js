9d8d82fb7f8256ff30eeeb8d94038c34fc9f4705
BenediktPago <144164566+BenediktPago@users.noreply.github.com>
2026-01-09 21:27:35 +0000

    Deploying to gh-pages from @ BenediktPago/BenediktPago.github.io@e17ce5fadd9dc495d738ee42c667ed760a8b7a5a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -388,7 +388,7 @@ ninja.data = [{
           title: 'Symmetric Proofs in the Ideal Proof System',
           description: "Oxford Proof Complexity Workshop 2025",
           section: "Projects",handler: () => {
-              window.location.href = "/assets/pdf/slidesPC25workshopHandout.pdf";
+              window.location.href = "/assets/pdf/talks/slidesPC25workshopHandout2.pdf";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
