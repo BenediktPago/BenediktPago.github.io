cfc4a00593817c490cf3b705acaf5943712d46e7
BenediktPago <144164566+BenediktPago@users.noreply.github.com>
2026-01-10 17:20:11 +0000

    Deploying to gh-pages from @ BenediktPago/BenediktPago.github.io@568c929c675838d9738b83dcb44ad2ac6ab73448 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -389,9 +389,9 @@ ninja.data = [{
           description: "Oxford Proof Complexity Workshop 2025",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-limitations-of-affine-csp-algorithms",
+          title: 'Limitations of Affine CSP Algorithms',
+          description: "ICALP 2025, Aarhus",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
